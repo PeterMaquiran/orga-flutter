5a67c5456483c7f3fa368bc9d97147e24ac8875a
Peter Maquiran <maquiranpeter29@gmail.com>
2026-03-13 11:58:08 +0100

    add theme

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:go_router/go_router.dart';
 import 'package:orga/ui/screen/ios/auth/email_confirmation.dart';
 import 'package:orga/ui/screen/ios/auth/login.dart';
 import 'package:orga/ui/screen/ios/home/home.dart';
+import 'package:orga/ui/shared/theme/appColors.dart';
 
 // 1. Define your Route "Map" (The Central Command)
 final GoRouter _router = GoRouter(
@@ -35,10 +36,10 @@ class OrgaApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // 2. Use .router constructor to connect your Map to the App
     return MaterialApp.router(
+      theme: appTheme,
       routerConfig: _router,
       title: 'Orga',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(brightness: Brightness.light),
     );
   }
 }
